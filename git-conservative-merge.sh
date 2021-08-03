fed73620099cde09bf4803832b34476fab73385a
davips <dpsabc@gmail.com>
2021-08-03 20:34:39 -0300

    Example

--- git-conservative-merge.sh
+++ git-conservative-merge.sh
@@ -1,7 +1,7 @@
 if [[ $# -ne 2 ]]; then
-    echo "Usage: git-conservative-merge.sh <our branch> <their branch>"
+    echo "Usage:    git-conservative-merge.sh <our branch> <their branch>"
+    echo "Example:  git-conservative-merge.sh main origin/main"
     exit
-    read -p "Press Ctrl+C to try again..."
 fi
 
 git checkout $1 
